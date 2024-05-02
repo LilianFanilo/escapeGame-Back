7b9dc11092ea93f3ba8cb65bd151b385373cc07a
Lilian <lilianpeuron@gmail.com>
2024-05-03 00:37:48 +0200

    Init Project

---
+++ example_index.js
@@ -0,0 +1,37 @@
+import bcrypt, { hash } from "bcrypt";
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+const users = [];
+
+app.post("/signup", async (req, res) => {
+  const { username, password } = req.body;
+  const hash = await bcrypt.hash(password, 13);
+  users.push({
+    username,
+    password: hash,
+  });
+
+  console.log(users);
+  res.send("ok");
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find((u) => u.username === username);
+  if (!user) {
+    res.send("wrong user");
+    return;
+  }
+  const isValid = await bcrypt.compare(password, user.password);
+  if (!isValid) {
+    res.send("wrong password");
+    return;
+  }
+
+  res.send("ok");
+});
+
+app.listen(8080, () => console.log("listening on port 8080"));
---
+++ index.js
@@ -0,0 +1,34 @@
+import bcrypt, { hash } from "bcrypt";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const users = [
+  {
+    username: "elodie",
+    password: "$2b$13$vmZTL3X3B7JPnChxf8elleECAUfG4Ei0votqRy4vi2NQhV9eieH3e",
+  },
+];
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find((u) => u.username === username);
+  if (!user) {
+    res.json({ success: false, message: "wrong user" });
+    return;
+  }
+  const isValid = await bcrypt.compare(password, user.password);
+  if (!isValid) {
+    res.json({ success: false, message: "wrong password" });
+    return;
+  }
+
+  res.json({ success: true, message: "login success" });
+  console.log(users);
+});
+
+app.listen(8080, () => console.log("listening on port 8080"));
